da1a645990e06e232bb573a9b103e8ac86caa33b
rcowin <richard.cowin@gmail.com>
2021-09-13 11:20:30 -0500

    add spa support for audience attribute updating

--- audience/audience.js
+++ audience/audience.js
@@ -1,7 +1,10 @@
+
 var audience = {
 }
 function refreshAudience(){
-  window.evolv.context.update(audience);
+  try{
+    window.evolv.context.update(audience);
+  } catch(e){console.info('evolv context not available')}
 }
 
 var MacroSet = {
@@ -50,7 +53,7 @@ var adapters = {
           else
             result = undefined;
         }
-        catch{}
+        catch(e){}
       } else {
         if (!result[token] && tokens.length > 0){ //try flattened object index
           token = token + '.' + tokens[0]
@@ -150,12 +153,71 @@ function initConfig(){
 };
 
 
+
+var Spa = {
+  queue: [],
+  addSpaListener: function(topKey, variable, audObj){
+    this.queue.push({
+      topKey: topKey,
+      variable: variable,
+      audObj: audObj
+    });
+  },
+  recheck: function(force){
+    var priorAudience = Object.assign({}, audience);
+    var prevLength = this.recheckQueue.length
+    
+    this.recheckQueue.forEach(function(l){
+      addAudience(l.topKey, l.variable, l.audObj);
+    });
+    this.recheckQueue = this.recheckQueue.filter(function(l){
+      return priorAudience[l.topKey][l.variable] === audience[l.topKey][l.variable];
+    });
+    if (force || this.recheckQueue.length < prevLength){
+        refreshAudience();
+    }
+    
+    console.info('finishing spa update', this.recheckQueue, priorAudience, audience)
+
+    if (this.recheckQueue.length === 0){
+      this.terminateRecheck();
+    }
+  },
+  terminateRecheck: function(){
+      console.info('recheck is ended', this.recheckQueue)
+    clearInterval(this.recheckInterval);
+    this.recheckInterval = 0;
+  },
+  eventHandler: function(){
+    if (this.queue === 0) return;
+
+    audience.config.spaNav = 'true'; //
+
+    this.recheckQueue = this.queue.slice(0); 
+    this.recheck(true);
+//    setTimeout(this.recheck.bind(this),0);//
+    this.recheckInterval = setInterval(this.recheck.bind(this), 25)
+    setTimeout(this.terminateRecheck.bind(this), 250)
+  },
+  initListener: function(){
+    var listener = this.eventHandler.bind(this);
+    window.addEventListener('popstate', listener);
+    window.addEventListener('stateupdate_evolv', listener);
+  }
+}
+
+Spa.initListener();
+
 function addAudience(topKey, key, obj){
   function bindAudienceValue(val, inc){
-    audience[topKey][key] = convertValue(val);
+    var newVal = convertValue(val);
+    if (audience[topKey][key] ===  newVal) return false;
+
+    audience[topKey][key] = newVal;
     if (inc){
        audience[topKey][key + '_pollingCount'] = '' + inc;
     }
+    return true;
   }
   try{
     var val = getValue(obj);
@@ -210,6 +272,9 @@ function setAudience(json){
         var audObj = namespace[variable]
         if (!audObj.page || new RegExp(audObj.page).test(location.pathname)) {
           addAudience(topKey, variable, audObj)
+          if (audObj.spa){
+            Spa.addSpaListener(topKey, variable, audObj);
+          }
         }
       })
     })
@@ -218,5 +283,6 @@ function setAudience(json){
 }
 
 audience.config = initConfig();
+
 
 module.exports = setAudience;

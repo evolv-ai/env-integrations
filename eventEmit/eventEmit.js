97a2d95430bc47ac74120fbb4ee01c62dcefb07f
rcowin <richard.cowin@gmail.com>
2021-10-18 13:31:47 -0500

    adding spa support to event emit

--- eventEmit/eventEmit.js
+++ eventEmit/eventEmit.js
@@ -1,4 +1,58 @@
 
+var Spa = {
+  queue: [],
+  addSpaListener: function(topKey, variable, audObj){
+    this.queue.push({
+      topKey: topKey,
+      variable: variable,
+      audObj: audObj
+    });
+  },
+  recheck: function(force){
+    var priorAudience = Object.assign({}, audience);
+    var prevLength = this.recheckQueue.length
+    
+    this.recheckQueue.forEach(function(l){
+      addAudience(l.topKey, l.variable, l.audObj);
+    });
+    this.recheckQueue = this.recheckQueue.filter(function(l){
+      return priorAudience[l.topKey][l.variable] === audience[l.topKey][l.variable];
+    });
+    if (force || this.recheckQueue.length < prevLength){
+        refreshAudience();
+    }
+    
+    console.info('finishing spa update', this.recheckQueue, priorAudience, audience)
+
+    if (this.recheckQueue.length === 0){
+      this.terminateRecheck();
+    }
+  },
+  terminateRecheck: function(){
+      console.info('recheck is ended', this.recheckQueue)
+    clearInterval(this.recheckInterval);
+    this.recheckInterval = 0;
+  },
+  eventHandler: function(){
+    if (this.queue === 0) return;
+
+    audience.config.spaNav = 'true'; //
+
+    this.recheckQueue = this.queue.slice(0); 
+    this.recheck(true);
+//    setTimeout(this.recheck.bind(this),0);//
+    this.recheckInterval = setInterval(this.recheck.bind(this), 25)
+    setTimeout(this.terminateRecheck.bind(this), 250)
+  },
+  initListener: function(){
+    var listener = this.eventHandler.bind(this);
+    window.addEventListener('popstate', listener);
+    window.addEventListener('stateupdate_evolv', listener);
+  }
+}
+
+Spa.initListener();
+
 
 function sendEvent(tag, url){
   try {
@@ -27,6 +81,7 @@ function processNav(config){
   })
 }
 
+module.exports = processNav
 
 // {
 //   "orderConfirmation.load": {
--- mvoRender/mvoRender.js
+++ mvoRender/mvoRender.js
@@ -26,7 +26,7 @@ function initializeRender(){
       }
     } else if (sel.constructor === HTMLDivElement){
       return [sel]
-    } else if (sel.constructor === Node){
+    } else if (sel.constructor === ENode){
       return sel.el
     } else if (Array.isArray(sel)){
       return sel
@@ -34,27 +34,27 @@ function initializeRender(){
       return [];
     }
   }
-  var Node = function(sel, context){
+  var ENode = function(sel, context){
     context = context || document;
     var el = toNodeValue(sel, context);
     this.el = Array.prototype.slice.call(el)
     this.length = this.el.length;
   }
-  Node.prototype.find = function(sel){
+  ENode.prototype.find = function(sel){
     var el = this.el
-    return new Node(el.map(function(e){
+    return new ENode(el.map(function(e){
       return Array.prototype.slice.call(toNodeValue(sel, e))
     }).flat(2))
   }
-  Node.prototype.parent = function(){
+  ENode.prototype.parent = function(){
     var el = this.el
     var parents = el.map(function(e){return e.parentNode})
     parents = parents.filter(function(item, pos){
         return parents.indexOf(item) == pos; 
       });
-    return new Node(parents)
+    return new ENode(parents)
   }
-  Node.prototype.addClass = function(classString){
+  ENode.prototype.addClass = function(classString){
     this.el.forEach(function(e){
       classString.split(' ').forEach(function(className){
         e.classList.add(className)
@@ -62,12 +62,12 @@ function initializeRender(){
     })
     return this;
   }
-  Node.prototype.removeClass = function(className){
+  ENode.prototype.removeClass = function(className){
     function removeTheClass(e){e.classList.remove(className)}
     this.el.forEach(removeTheClass)
     return this;
   }
-  Node.prototype.append = function(item){
+  ENode.prototype.append = function(item){
     var node = this.el[0];
     if (!node) return;
 
@@ -76,36 +76,38 @@ function initializeRender(){
 
     return this;
   }
-  Node.prototype.insertBefore = function(item){
-    node = this.el[0];
+  ENode.prototype.insertBefore = function(item){
+    var node = this.el[0];
     if (!node) {
       console.info('no content for insert') 
       return this;
     }
     if (typeof item === 'string'){
       item = document.querySelectorAll(item);
-    } else if (item.constructor === Node){
+    } else if (item.constructor === ENode){
       item = item.el[0]
     }
     item.parentNode.insertBefore(node, item);
     return this;
   }
-  Node.prototype.markOnce = function(attr){
+  ENode.prototype.markOnce = function(attr){
     var results = this.el.filter(function(e){
       return !e.getAttribute(attr);
     })
     results.forEach(function(e){e.setAttribute(attr, true)})
-    return new Node(results)
+    return new ENode(results)
   }
-  Node.prototype.on = function(tag, fnc){
+  ENode.prototype.on = function(tag, fnc){
     this.el.forEach(function(e){e.addEventListener(tag,fnc)});
     return this;
   }
-  Node.prototype.html = function(str){
+  ENode.prototype.html = function(str){
+    if (!str) this.el.map(function(e){return e.innerHTML});
+ 
     this.el.forEach(function(e){e.innerHTML = str;});
     return this;
   }
-  Node.prototype.attr = function(attributes){
+  ENode.prototype.attr = function(attributes){
     this.el.forEach(function(e){
       var keys = Object.keys(attributes);
       keys.forEach(function(key){
@@ -114,15 +116,44 @@ function initializeRender(){
     })
     return this;
   }
+  ENode.prototype.watch = function(options){
+    var config = Object.assign({}, options || {}, {    
+      attributes: true,
+      childList: true,
+      characterData: false,
+      subtree: false
+    });
+    var cb;
+    var observer = new MutationObserver(function (mutations) {
+      cb(mutations);
+    });
+    this.el.forEach(function(e){ observer.observe(e, config)});
+    return {then: function(fnc){
+      cb = fnc;
+    }}
+  }
+  ENode.prototype.firstDom = function(){
+    return this.el[0];
+  }
+  ENode.prototype.lastDom = function(){
+    return this.el.slice(-1)[0]
+  }
+  ENode.prototype.first = function(){
+    return new ENode(this.firstDom());
+  }
+  ENode.prototype.last = function(){
+    return new ENode(this.lastDom());
+  }
+
   var $ = function(sel){
-    return new Node(sel);
+    return new ENode(sel);
   }
 
   //rule instrumentation package
   var store = {
     instrumentDOM: function(data){
       if (data){
-        store.cache = data;
+        store.cache = Object.assign(store.cache || {}, data || {});
       } else {
         data = store.cache;
       }
@@ -141,11 +172,11 @@ function initializeRender(){
     }
   }
 
-  var rule = window._evolvRule = {
+  var rule = window.evolv.renderRule = window.evolv.renderRule || {
     $: $,
     exp: 'g',
     store: store,
-    when: function (trig) {
+    when: function(trig){
       var variant = null;
       var hasTriggered = false;
       var gdom = null;
@@ -170,30 +201,6 @@ function initializeRender(){
         return results.length > 0 ? results : null;
       }))
     },
-    intervalTrigger: function (selFnc) {
-      var int = 80;
-      var dur = 20000;
-      return function(fnc){
-        function render() {
-          var dom = selFnc();
-          if (dom) {
-            try{
-              fnc(dom);
-            } catch(e){
-              console.warn('failed variant', e)
-              window.evolv.client.contaminate({reason:'variant-failed', details: e}, true)
-            }
-            clearInterval(interval);
-          }
-        }
-        var interval = setInterval(render, int);
-        $(function(){setTimeout(render,200)})
-        setTimeout(function () {
-          clearInterval(interval);
-        }, dur);
-      };
-    },
-      
     intervalTriggerQueue: null,
     queuedIntervalTrigger: function(selFnc){
       if (!this.intervalTriggerQueue){
@@ -241,12 +248,22 @@ function initializeRender(){
       }
     }
   }
+  rule.intervalTrigger = rule.queuedIntervalTrigger;
+  rule.trigger = rule.queuedIntervalTrigger;
 
   var validate = {
     lengthGreaterThan: function(val){
       return function(dom){ return dom.length >= val}
+    },
+    contains: function(val){
+      return function(dom){ return !!dom.find(val)}
+    },
+    notContains: function(val){
+      return function(dom){ return !dom.find(val)}
     }
   }
+
+  rule.validate = validate;
 }
 
 
@@ -264,3 +281,6 @@ function processNav(config){
     initializeRender();
   }
 }
+
+
+module.exports = processNav

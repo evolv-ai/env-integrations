4ae1f7dbf8b57ef99a070ca6e25459b1ba7ede81
rcowin <richard.cowin@gmail.com>
2021-10-25 18:31:10 -0500

    support reactivate with watch

--- mvoRender/mvoRender.js
+++ mvoRender/mvoRender.js
@@ -1,5 +1,8 @@
+window.evolv = {};
+
 
 function initializeRender(){
+  if (window.evolv.renderRule) return;
 
   //debounce code
   function debounce (fn, dur) {
@@ -46,6 +49,12 @@ function initializeRender(){
       return Array.prototype.slice.call(toNodeValue(sel, e))
     }).flat(2))
   }
+  ENode.prototype.closest = function(sel){
+    var el = this.el
+    return new ENode(el.map(function(e){
+      return e.closest(sel)
+    }))
+  }
   ENode.prototype.parent = function(){
     var el = this.el
     var parents = el.map(function(e){return e.parentNode})
@@ -54,6 +63,10 @@ function initializeRender(){
       });
     return new ENode(parents)
   }
+  ENode.prototype.contains = function (text) {
+    var el = this.el
+    return new ENode(el.filter(function (e) { return e.textContent.includes(text)}));
+ }
   ENode.prototype.addClass = function(classString){
     this.el.forEach(function(e){
       classString.split(' ').forEach(function(className){
@@ -118,14 +131,14 @@ function initializeRender(){
   }
   ENode.prototype.watch = function(options){
     var config = Object.assign({}, options || {}, {    
-      attributes: true,
+      attributes: false,
       childList: true,
       characterData: false,
       subtree: false
     });
     var cb;
     var observer = new MutationObserver(function (mutations) {
-      cb(mutations);
+      if (cb) cb(mutations);
     });
     this.el.forEach(function(e){ observer.observe(e, config)});
     return {then: function(fnc){
@@ -149,66 +162,128 @@ function initializeRender(){
     return new ENode(sel);
   }
 
-  //rule instrumentation package
-  var store = {
-    instrumentDOM: function(data){
-      if (data){
-        store.cache = Object.assign(store.cache || {}, data || {});
-      } else {
-        data = store.cache;
+  var renderRule = {}
+  let renderRuleProxy = new Proxy(renderRule, {
+    get(target, name, receiver) {
+        let rv = Reflect.get(target, name, receiver);
+        if (!rv) {
+            target[name] = initializeExperimentRule(name); 
+            rv = Reflect.get(target, name, receiver)
+        }
+        return rv;
       }
-      var check = function(obj){
-        rule
-          .when(rule.trigger(function(){ 
-            return obj.dom.length >= obj.count || 1
-          }))
-          .then(function(){
-            obj.node = obj.dom.addClass('evolv-' + obj.asClass)
-          })
-      }
-      for(var key in data){
-        check(data[key])     
+  });
+
+  //enables creating a rule context via window.evolv.renderRule.myRule
+  window.evolv.renderRule = renderRuleProxy;
+
+  function initializeExperimentRule(name){
+    var store = {
+      reactivate: function(){
+        rule.reactivate();
+      },
+      instrumentDOM: function(data){
+        if (data){
+          store.cache = Object.assign(store.cache || {}, data || {});
+        } else {
+          data = store.cache;
+        }
+        var check = function(obj){
+          rule
+            .when(rule.trigger(function(){ 
+              return obj.dom.length >= (obj.count || 1)
+            }))
+            .then(function(){
+              obj.node = obj.dom.addClass('evolv-' + obj.asClass)
+            })
+        }
+        for(var key in data){
+          check(data[key])     
+        }
       }
     }
-  }
 
-  var rule = window.evolv.renderRule = window.evolv.renderRule || {
-    $: $,
-    exp: 'g',
-    store: store,
-    when: function(trig){
-      var variant = null;
-      var hasTriggered = false;
-      var gdom = null;
-      trig(function (dom) {
-        gdom = dom;
-        hasTriggered = true;
-        if (variant) variant(dom);
-      });
-      return {
-        then: function (fnc) {
-          variant = fnc;
-          if (hasTriggered) variant(gdom);
-        },
-      };
-    },
-    nextIndex: 0,
-    whenDOM: function(sel){
-      var index = rule.nextIndex++
-      return rule.when(rule.trigger(function() {
-        var attr = 'evolv-'+ rule.exp + index;
-        var results = $(sel).markOnce(attr)
-        return results.length > 0 ? results : null;
-      }))
-    },
-    intervalTriggerQueue: null,
-    queuedIntervalTrigger: function(selFnc){
-      if (!this.intervalTriggerQueue){
-        this.intervalTriggerQueue = []
+    var rule = {
+      fns: {},
+      $: $,
+      exp: name,
+      store: store,
+      when: function(trig){
+        var variant = null;
+        var hasTriggered = false;
+        var gdom = null;
+        trig(function (dom) {
+          gdom = dom;
+          hasTriggered = true;
+          if (variant) variant(dom);
+        });
+        return {
+          then: function (fnc) {
+            variant = fnc;
+            if (hasTriggered) variant(gdom);
+          },
+        };
+      },
+      nextIndex: 0,
+      whenDOM: function(sel){
+        var index = rule.nextIndex++
+        return rule.when(rule.trigger(function() {
+          var attr = 'evolv-'+ rule.exp + index;
+          var results = $(sel).markOnce(attr)
+          return results.length > 0 ? results : null;
+        }))
+      },
+      trigger: function(selFnc){
+        return this.triggerHandler.trigger(selFnc)
+      },
+      reactivate: function(){
+        return this.triggerHandler.reactivate()
+      }
+    };
+
+    var queuedIntervalHandler = {
+      triggerHistoryQueue: [],
+      triggerQueue: null,
+      trigger: function(selFnc){
+        if (!this.triggerQueue){
+          this.triggerQueue = []
+          this.startProcessing();
+        }
+
+        var task = this.createTask(selFnc);
+        this.triggerQueue.push(task)
+        this.triggerHistoryQueue.push(task)
+
+        return function(fnc){
+          task.action = fnc;
+        }
+      },
+      reactivate: function(){
+        this.triggerQueue = this.triggerHistoryQueue.slice(0);
+        this.startProcessing(500);
+      },
+      createTask: function(selFnc){
+        return {
+          selFnc: selFnc,
+          action: function(){}
+        }
+      },
+      intervalTimer : null,
+      clearIntervalTimer: function(){
+        if (this.intervalTimer){
+          clearInterval(this.intervalTimer);
+          this.intervalTimer = null
+        }
+      },
+      startProcessing: function(duration){
         var int = 60;
-        var dur = 20000;
+        var dur = duration || 10000;
+
+        this.clearIntervalTimer();
+    
         function process() {
-          var items = this.intervalTriggerQueue.slice()
+          var items = this.triggerQueue.slice()
+
           var results;
           do {
             items = results || items
@@ -226,46 +301,44 @@ function initializeRender(){
             })
           } while(items.length > results.length)
 
-          this.intervalTriggerQueue = results;
+          
+          this.triggerQueue = results;
           if (results.length === 0){
-            clearInterval(interval);
-            interval = null;
+            setTimeout(function(){ //give other calls a chance to join before we end this.
+              if (this.triggerQueue.length !== 0) return;
+
+              this.clearIntervalTimer();
+            }.bind(this), 30);
           }
         }
-        var interval = setInterval(process.bind(this), int);
-        $(function(){setTimeout(process.bind(this),1)})
-        setTimeout(function () {
-          clearInterval(interval);
-        }, dur);
-      }
-      var task = {
-        selFnc: selFnc,
-        action: function(){}
-      }
-      this.intervalTriggerQueue.push(task)
-      return function(fnc){
-        task.action = fnc;
+
+          this.intervalTimer = setInterval(process.bind(this), int);
+          $(function(){setTimeout(process.bind(this),1)})
+          setTimeout(this.clearIntervalTimer.bind(this), dur);
       }
     }
-  }
-  rule.intervalTrigger = rule.queuedIntervalTrigger;
-  rule.trigger = rule.queuedIntervalTrigger;
 
-  var validate = {
-    lengthGreaterThan: function(val){
-      return function(dom){ return dom.length >= val}
-    },
-    contains: function(val){
-      return function(dom){ return !!dom.find(val)}
-    },
-    notContains: function(val){
-      return function(dom){ return !dom.find(val)}
+    rule.triggerHandler = queuedIntervalHandler;
+
+    var validate = {
+      lengthGreaterThan: function(val){
+        return function(dom){ return dom.length >= val}
+      },
+      contains: function(val){
+        return function(dom){ return !!dom.find(val)}
+      },
+      notContains: function(val){
+        return function(dom){ return !dom.find(val)}
+      }
     }
-  }
 
-  rule.validate = validate;
+    rule.validate = validate;
+
+    return rule;
+  }
 }
 
+//initializeRender();
 
 function pageMatch(page){
   if (!page) return false;
@@ -283,4 +356,8 @@ function processNav(config){
 }
 
 
-module.exports = processNav
+//toggle the following comments to enable directly in experiment code
+
+//processNav({pages: ['.*']});
+module.exports = processNav;
+

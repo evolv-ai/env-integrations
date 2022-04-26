7ab99ace35067420506d73acd97371c45f39a7b5
rcowin <richard.cowin@gmail.com>
2022-04-26 14:24:25 -0500

    make all extensions deferred - prevents multiple analytics messages

--- analytics/src/waitFor.js
+++ analytics/src/waitFor.js
@@ -2,20 +2,21 @@ export function waitFor(check, invoke, poll){
   if (check()){
       invoke();
       return;
+  } else {
+    var polling = setInterval(function(){
+      try{
+        if (check()){
+          invoke();
+          clearInterval(polling);
+          polling = null;
+        }
+      } catch(e){console.info('Evolv: Listener not processed')}
+    }, poll.interval)
+    setTimeout(function(){ 
+        if (!polling) return
+        
+        clearInterval(polling)     
+        console.info('Evolv: Listener timeout')
+    }, poll.duration)
   }
-  var polling = setInterval(function(){
-    try{
-      if (check()){
-        invoke();
-        clearInterval(polling);
-        polling = null;
-      }
-    } catch(e){console.info('Evolv: Listener not processed')}
-  }, poll.interval)
-  setTimeout(function(){ 
-      if (!polling) return
-      
-      clearInterval(polling)     
-      console.info('Evolv: Listener timeout')
-  }, poll.duration)
 }
--- analytics/test/testConfig.js
+++ analytics/test/testConfig.js
@@ -1,5 +1,5 @@
 import {processAnalytics} from '../src/analytics.js'
-import data from './testData.json'
-//import data from './data.json'
+//import data from './testdata.json'
+import data from './data.json'
 
 setTimeout(()=>processAnalytics(data),100);

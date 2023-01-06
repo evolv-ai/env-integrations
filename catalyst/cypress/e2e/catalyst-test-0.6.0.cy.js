22e961d5442ad5bba7ad679cb0b768fd7ff771a3
Charles Robertson <charles.robertson@evolv.ai>
2023-01-06 15:16:19 -0500

    Add start of 0.6.0 E2E test

---
+++ catalyst/cypress/e2e/catalyst-test-0.6.0.cy.js
@@ -0,0 +1,264 @@
+describe('Catalyst 0.6.0+ E2E Test', () => {
+    before(function () {
+        cy.visit('http://localhost:3334/catalyst-0-6-0/');
+    });
+
+    after(function () {
+        // runs once after the last test in this block
+    });
+
+    beforeEach(function () {
+        // runs before each test in this block
+    });
+
+    afterEach(function () {
+        // runs after each test in this block
+    });
+
+    it('Confirm homepage DOM is instrumented on initial page load', () => {
+        cy.get('.evolv-body').should('exist');
+        cy.get('.evolv-learnButton').should('exist');
+        cy.get('.evolv-jumbotron').should('exist');
+        cy.get('.evolv-navItem').should('exist');
+        cy.get('.evolv-navItem').should('have.length', 7);
+    });
+
+    it('Confirm rule.select, rule.selectAll, rule.selectInstrument work', () => {
+        cy.get('.evolv-select-first-h2')
+            .should('exist')
+            .should('have.length', 1);
+        cy.get('.evolv-select-all-p').should('exist').should('have.length', 13);
+        cy.get('.evolv-select-single-instrument')
+            .should('exist')
+            .should('have.length', 1);
+        cy.get('.evolv-select-all-instrument')
+            .should('exist')
+            .should('have.length', 7);
+    });
+
+    //     it('Confirm "whenDOM" works', () => {
+    //         cy.get('body.whenDOM_tagged').should('exist');
+    //         cy.get('.evolv-learnButton').contains('Learn things');
+    //     });
+
+    //     it('Confirm "whenItem" works', () => {
+    //         cy.get('.evolv-bottomContainerSection.whenItem_tagged').should('exist');
+    //         cy.get('.evolv-bottomContainerSectionH2.whenItem_tagged').should(
+    //             'have.length',
+    //             3
+    //         );
+
+    //         cy.get('.evolv-bottomContainerSectionH2_first').should('exist');
+    //         cy.get('.evolv-bottomContainerSectionH2_second').should('exist');
+    //         cy.get('.evolv-bottomContainerSectionH2_third').should('exist');
+
+    //         cy.get('.evolv-bottomContainerSectionH2_first').contains('Headline 1');
+    //         cy.get('.evolv-bottomContainerSectionH2_second').contains('Headline 2');
+    //         cy.get('.evolv-bottomContainerSectionH2_third').contains('Headline 3');
+    //     });
+
+    //     it('Confirm ".parent()" works', () => {
+    //         cy.get('.evolv-jumbotronH1Parent').should('exist');
+    //     });
+
+    //     ///////////////////
+    //     // Helper functions
+    //     ///////////////////
+
+    //     it('Confirm ".filter()" works', () => {
+    //         cy.get('.evolv-bottomContainerSection_filtered')
+    //             .should('exist')
+    //             .should('have.length', 2);
+    //     });
+
+    //     it('Confirm ".contains()" works', () => {
+    //         cy.get('.evolv-containsCheckout').should('exist').contains('Checkout');
+    //     });
+
+    //     it('Confirm ".find()" works', () => {
+    //         cy.get('.evolv-findSecondDetailsButton')
+    //             .should('exist')
+    //             .contains('View details');
+    //     });
+
+    //     it('Confirm ".closest()" works', () => {
+    //         cy.get('.evolv-closestRow').should('exist').should('have.class', 'row');
+    //     });
+
+    //     it('Confirm ".children()" works', () => {
+    //         cy.get('.evolv-rowChildrenNotFirst')
+    //             .should('exist')
+    //             .should('have.length', 2);
+    //     });
+
+    //     it('Confirm ".next()" works', () => {
+    //         cy.get('.evolv-nextSection')
+    //             .should('exist')
+    //             .should('have.length', 1)
+    //             .contains('Headline 2');
+    //     });
+
+    //     it('Confirm ".prev()" works when', () => {
+    //         cy.get('.evolv-prevSection')
+    //             .should('exist')
+    //             .should('have.length', 1)
+    //             .contains('Headline 1');
+    //     });
+
+    //     it('Confirm ".addClass()" works', () => {
+    //         cy.get('h1').should('have.class', 'evolv-mainHeading');
+    //     });
+
+    //     it('Confirm ".removeClass()" works', () => {
+    //         cy.get('footer > p')
+    //             .should('exist')
+    //             .should('not.have.class', 'copyright');
+    //     });
+
+    //     it('Confirm ".append()" works', () => {
+    //         cy.get('.footer-append')
+    //             .should('exist')
+    //             .contains('Testing by Charles Robertson');
+    //     });
+
+    //     it('Confirm ".prepend()" works', () => {
+    //         cy.get('.footer-prepend').should('exist').contains('Additional info:');
+    //     });
+
+    //     it('Confirm ".beforeMe()" works', () => {
+    //         cy.get('.number-gallery-title')
+    //             .should('exist')
+    //             .contains('Number Gallery');
+    //     });
+
+    //     it('Confirm ".afterMe()" works', () => {
+    //         cy.get('.section-nonexistant').should('exist').contains('but it does');
+    //     });
+
+    //     it('Confirm ".insertBefore()" works', () => {
+    //         cy.get('.content-end').should('exist').contains('End');
+    //     });
+
+    //     it('Confirm ".insertAfter()" works', () => {
+    //         cy.get('.sub-footer').should('exist').contains('below');
+    //     });
+
+    //     it('Confirm ".wrap()" works', () => {
+    //         cy.get('.number-gallery-wrap')
+    //             .should('exist')
+    //             .children()
+    //             .first()
+    //             .should('have.class', 'number-gallery');
+    //     });
+
+    //     it('Confirm ".wrapAll()" works', () => {
+    //         cy.get('.number-gallery-inner')
+    //             .should('exist')
+    //             .children()
+    //             .first('have.class', 'number-gallery-item');
+    //     });
+
+    //     it('Confirm ".markOnce()" works', () => {
+    //         cy.get('.number-gallery-item[marked="true"]')
+    //             .should('exist')
+    //             .should('have.length', 3);
+    //     });
+
+    //     it('Confirm ".on()" works', () => {
+    //         cy.get('h1').click().should('have.attr', 'evolv-click');
+    //     });
+
+    //     it('Confirm ".html()" works when passed a string', () => {
+    //         cy.get('.html-test-string-heading')
+    //             .should('exist')
+    //             .contains('New Section');
+    //     });
+
+    //     it('Confirm ".html()" works for outputting a string', () => {
+    //         cy.get('.number-gallery-markup')
+    //             .should('exist')
+    //             .contains('<div class="number-gallery-item">1</div>');
+    //     });
+
+    //     it('Confirm ".text()" works when passed a string', () => {
+    //         cy.get('.text-output').contains('This is a template');
+    //     });
+
+    //     it('Confirm ".text()" works for outputting a string', () => {
+    //         cy.get('.number-gallery-item:nth-child(6)').contains('six');
+    //     });
+
+    //     it('Confirm ".attr()" works when getting an attribute', () => {
+    //         cy.get('.attr-output').contains('test');
+    //     });
+
+    //     it('Confirm ".attr()" works for setting an attribute', () => {
+    //         cy.get('.number-gallery').should('have.attr', 'evolv-count', '6');
+    //     });
+
+    //     it('Confirm ".each()" works', () => {
+    //         cy.get('.number-gallery-item').first().contains('*');
+    //     });
+
+    //     it('Confirm ".watch()" works', () => {
+    //         cy.get('.number-gallery-item:nth-child(7)')
+    //             .should('exist')
+    //             .should('have.css', 'background-color')
+    //             .should('eq', 'rgb(78, 132, 138)');
+    //     });
+
+    //     it('Confirm ".firstDom()" works', () => {
+    //         cy.get('.number-gallery-item:first-child')
+    //             .should('have.css', 'background-color')
+    //             .should('eq', 'rgb(183, 63, 63)');
+    //     });
+
+    //     it('Confirm ".lastDom()" works', () => {
+    //         cy.get('.number-gallery-item:nth-child(6)')
+    //             .should('have.css', 'background-color')
+    //             .should('eq', 'rgb(183, 63, 63)');
+    //     });
+
+    //     it('Confirm ".first()" works', () => {
+    //         cy.get('.number-gallery-item:first-child')
+    //             .should('have.css', 'font-weight')
+    //             .should('eq', '700');
+    //     });
+
+    //     it('Confirm ".last()" works', () => {
+    //         cy.get('.number-gallery-item:nth-child(6)')
+    //             .should('have.css', 'font-weight')
+    //             .should('eq', '700');
+    //     });
+
+    //     it('Confirm "rule.reactivate" works', () => {
+    //         cy.get('[class*="evolv-"]').should('have.length', 23);
+    //         cy.window()
+    //             .then((window) => {
+    //                 const instrumented =
+    //                     window.document.querySelectorAll('[class*="evolv-"]');
+    //                 instrumented.forEach((element) => {
+    //                     const classes = Array.from(element.classList).filter(
+    //                         (a) => a.indexOf('evolv-') === 0
+    //                     );
+    //                     element.classList.remove(...classes);
+    //                 });
+    //             })
+    //             .then((a) => {
+    //                 cy.get('[class*="evolv-"]').should('have.length', 0);
+    //             });
+
+    //         cy.window().then((window) => {
+    //             window.evolv.renderRule.catalystTest.reactivate();
+    //         });
+
+    //         cy.get('[class*="evolv-"]').should('have.length', 23);
+    //     });
+
+    //     it('Confirm "rule.reactivateOnChange" works', () => {
+    //         cy.get('.tab-header .tab-2').click();
+    //         cy.get('.tab-panel-2 h3').should('have.class', 'evolv-tabPanelHeading');
+    //         cy.get('.tab-header .tab-1').click();
+    //         cy.get('.tab-panel-1 h3').should('have.class', 'evolv-tabPanelHeading');
+    //     });
+});
---
+++ catalyst/cypress/sandbox/catalyst-0-6-0/index.js
@@ -0,0 +1,357 @@
+setTimeout(() => {
+    const rule = window.evolv.catalyst['test-0-6-0'];
+    const store = rule.store;
+    const $ = rule.select;
+    const $$ = rule.selectAll;
+    const $i = rule.selectInstrument;
+    const { log, instrument } = rule;
+
+    instrument.add([
+        ['body', () => $('body'), { type: 'single' }],
+        ['navItem', () => $$('.nav-item')],
+        ['productNavItemLink', () => $$('.nav-item a[href="product.html"]')],
+        ['learnButton', () => $('#learn'), { type: 'single' }],
+        ['jumbotron', () => $('.jumbotron'), { type: 'single' }],
+        ['when-methods', () => $('.when-methods'), { type: 'single' }],
+    ]);
+
+    // select, selectAll, selectInstrument
+    $('h2').addClass('evolv-select-first-h2');
+    $$('//p').addClass('evolv-select-all-p');
+    $i('learnButton').addClass('evolv-select-single-instrument');
+    $i('navItem').addClass('evolv-select-all-instrument');
+
+    // rule.track
+    rule.track('c1v1');
+
+    // WHEN functions
+    // Here we can only test the active state because there is no live Evolv snippet and SPA routing setup
+    rule.whenContext('active').then(() => {
+        $('.when-methods .row').append(
+            `<div class="col-md-4">
+                    <h3>whenContext</h3>
+                    <p>The current context is '${rule._evolvContext.state.current}'</p>
+                </div>
+            </div>`
+        );
+    });
+
+    rule.whenMutate().then(() => {
+        $('.when-methods .row').markOnce('when-mutate')
+            .append(`<div class="col-md-4">
+                        <h3>whenMutate</h3>
+                        <p>A mutation occurred
+                    </p>
+                </div>`);
+    });
+
+    rule.whenItem('when-methods').then((whenMethods) => {
+        whenMethods.find('.row').append(`<div class="col-md-4">
+                <h3>whenItem</h3>
+                <p>Element instrumented
+            </p>
+        </div>`);
+    });
+
+    rule.whenDOM('.when-methods.delayed-class .row').then((row) =>
+        row.append(`<div class="col-md-4">
+            <h3>whenDOM</h3>
+            <p>The delayed class has now appeared
+        </p>
+        </div>`)
+    );
+
+    setTimeout(() => $('.evolv-when-methods').addClass('delayed-class'), 250);
+
+    rule.whenElement('.when-methods.delayed-class .row').then((row) =>
+        row.insertAdjacentHTML(
+            'beforeend',
+            `<div class="col-md-4">
+            <h3>whenElement</h3>
+            <p>The delayed class has now appeared
+        </p>
+        </div>`
+        )
+    );
+
+    let whenMethodsCount = 0;
+
+    rule.whenElements('.when-methods.delayed-class .row > div').then((div) => {
+        whenMethodsCount++;
+
+        $('.when-methods .row').markOnce('when-elements')
+            .append(`<div class="col-md-4">
+                <h3>whenElements</h3>
+                <p><span class="when-methods-count">${whenMethodsCount}</span> when methods found
+                </p>
+            </div>`);
+
+        $('.when-methods-count').text(whenMethodsCount);
+    });
+
+    store.instrumentDOM({
+        bottomContainerSection: {
+            get dom() {
+                return $('.container .row .col-md-4');
+            },
+        },
+        bottomContainerSectionH2: {
+            get dom() {
+                return $('.evolv-bottomContainerSection h2');
+            },
+        },
+        bottomContainerSectionH2_first: {
+            get dom() {
+                return $('.evolv-bottomContainerSection:nth-of-type(1) h2');
+            },
+        },
+        bottomContainerSectionH2_second: {
+            get dom() {
+                return $('.evolv-bottomContainerSection:nth-of-type(2) h2');
+            },
+        },
+        bottomContainerSectionH2_third: {
+            get dom() {
+                return $('.evolv-bottomContainerSection:nth-of-type(3) h2');
+            },
+        },
+        bottomContainerSection_filtered: {
+            get dom() {
+                return $('.row h2').filter(
+                    ':not(.evolv-bottomContainerSectionH2_third)'
+                );
+            },
+        },
+        jumbotronH1: {
+            get dom() {
+                return $('.jumbotron h1');
+            },
+        },
+        jumbotronH1Parent: {
+            get dom() {
+                return $('.evolv-jumbotronH1').parent();
+            },
+        },
+        containsCheckout: {
+            get dom() {
+                return $('button').contains('Checkout');
+            },
+        },
+        findSecondDetailsButton: {
+            get dom() {
+                return $('.row').find('.col-md-4:nth-child(2) .btn');
+            },
+        },
+        closestRow: {
+            get dom() {
+                return $('.col-md-4').closest('.row');
+            },
+        },
+        numberEight: {
+            get dom() {
+                return $('.number-gallery-item:nth-child(8)');
+            },
+        },
+        rowChildren: {
+            get dom() {
+                return $('.row').children();
+            },
+        },
+        rowChildrenNotFirst: {
+            get dom() {
+                return $('.row').children(':not(:first-child)');
+            },
+        },
+        nextSection: {
+            get dom() {
+                return $('.col-md-4:first-child').next();
+            },
+        },
+        prevSection: {
+            get dom() {
+                return $('.col-md-4:nth-child(2)').prev();
+            },
+        },
+        tabPanelHeading: {
+            get dom() {
+                return $('.tab-panels h3');
+            },
+        },
+    });
+
+    // Confirm "whenDOM" works
+    rule.whenDOM('.evolv-body').then(function (el1) {
+        var body = el1.firstDom();
+        body.classList.add('whenDOM_tagged');
+        body.insertAdjacentHTML('afterbegin', '<p>whenDOM was here</p>');
+
+        rule.whenDOM('.evolv-learnButton').then(function (el2) {
+            var learnButton = el2.firstDom();
+            learnButton.textContent = 'Learn things';
+        });
+    });
+
+    // Confirm "whenItem" works
+    rule.whenItem('bottomContainerSection').then(function (el1) {
+        var bottomContainerHeadline = el1.firstDom();
+        bottomContainerHeadline.classList.add('whenItem_tagged');
+
+        rule.whenItem('bottomContainerSectionH2').then(function (el2) {
+            var headlineEl = el2.firstDom();
+            headlineEl.classList.add('whenItem_tagged');
+        });
+    });
+    rule.whenItem('bottomContainerSectionH2_first').then(function (el) {
+        var headlineEl = el.firstDom();
+        headlineEl.textContent = 'Headline 1';
+    });
+    rule.whenItem('bottomContainerSectionH2_second').then(function (el) {
+        var headlineEl = el.firstDom();
+        headlineEl.textContent = 'Headline 2';
+    });
+    rule.whenItem('bottomContainerSectionH2_third').then(function (el) {
+        var headlineEl = el.firstDom();
+        headlineEl.textContent = 'Headline 3';
+    });
+
+    // Manipulate Class
+    rule.whenItem('jumbotronH1').then((ENode) => {
+        ENode.addClass('evolv-mainHeading');
+    });
+
+    $('.copyright').removeClass('copyright');
+
+    // Repositioning and insertion
+    $('footer').append(
+        $(
+            "<p class='footer-append'>Testing by Charles Robertson [ .append() ]</p>"
+        )
+    );
+    $('footer').prepend(
+        $("<p class='footer-prepend'>Additional info: [ .prepend() ]</p>")
+    );
+    // // insertBefore/insertAfter kill the footer
+    $(
+        "<p class='content-end'>End of content. [ .insertBefore() ]</p>"
+    ).insertBefore($('footer'));
+    $(
+        "<div class='sub-footer bg-dark' style='color: white'><p>What goes below the footer? [ .insertAfter() ]</p></div>"
+    ).insertAfter($('footer'));
+    $('.number-gallery').beforeMe(
+        "<h2 class='number-gallery-title'>Number Gallery: </div>"
+    );
+    $('.number-gallery').afterMe(
+        "<section class='section-nonexistant'><hr><h2>New Section</h2><p>This section probably shouldn't exist, but it does, thanks to the <code>.afterMe()</code> method and you're going to have to deal with that.</p></section>"
+    );
+
+    // wrap/wrapAll is not a function
+    $('.number-gallery').wrap("<div class='number-gallery-wrap'></div>");
+
+    $('.number-gallery-item').wrapAll(
+        "<div class='number-gallery-inner' style='display: grid; width: 100%; gap: .5em'></div>"
+    );
+    $('.number-gallery-item:nth-child(2n)').markOnce('marked');
+    $('body').on('click', (event) => {
+        console.log('evolv: click');
+        event.target.setAttribute('evolv-click', true);
+    });
+
+    $('.html-test-string').html(
+        "<h2 class='html-test-string-heading'>New Section</h2><p>This section was added by the ENode.prototype.html() method by passing a string. Its markup is below.</p><hr />"
+    );
+
+    document
+        .querySelector('.number-gallery-markup')
+        .append($('.number-gallery').html());
+
+    $('.number-gallery-item:nth-child(6)').text('six');
+    document.querySelector('.text-output').textContent =
+        $('.jumbotron p').text();
+
+    document.querySelector('.attr-output').textContent =
+        $('.number-gallery').attr('evolv-attr');
+    $('.number-gallery').attr({ 'evolv-count': '6' });
+
+    // Constructs
+
+    $('.number-gallery-item').each((item) => {
+        item.text(item.firstDom().textContent + '*');
+    });
+
+    // Watch
+    $('.number-gallery')
+        .watch()
+        .then((mutations) => {
+            mutations.forEach((mutation) => {
+                mutation.addedNodes.forEach((node) => {
+                    node.style.backgroundColor = 'rgb(78 132 138)';
+                });
+            });
+        });
+
+    setTimeout(() => {
+        let seven = document.createElement('div');
+        seven.classList.add('number-gallery-item');
+        seven.textContent = '7';
+        document.querySelector('.number-gallery-item:last-child').after(seven);
+    }, 100);
+
+    $('.number-gallery-item').firstDom().style.backgroundColor =
+        'rgb(183 63 63)';
+    $('.number-gallery-item').lastDom().style.backgroundColor =
+        'rgb(183 63 63)';
+    $('.number-gallery-item').first().el[0].style.fontWeight = 'bold';
+    $('.number-gallery-item').last().el[0].style.fontWeight = 'bold';
+
+    // Reactivate
+    // setTimeout(() => {
+    //   const evolvInstruments = document.querySelectorAll('[class*="evolv-"]');
+    //   evolvInstruments.forEach((element) => {
+    //     element.classList.forEach((classItem) => {
+    //       if (classItem.indexOf("evolv-") === 0) {
+    //         element.classList.remove(classItem);
+    //       }
+    //     });
+    //   });
+    // }, 1000);
+
+    // setTimeout(() => {
+    //   rule.reactivate();
+    // }, 1500);
+
+    rule.whenDOM('.tab-panels').reactivateOnChange();
+
+    // Reactivate on change
+    const tabSection = document.querySelector('.tabs');
+    const tabs = tabSection.querySelectorAll('.tab');
+    const tabPanels = [];
+    const tabPanelSection = tabSection.querySelector('.tab-panels');
+    tabPanels[0] = tabSection.querySelector('.tab-panel-1').cloneNode(true);
+    tabPanels[1] = tabSection.querySelector('.tab-panel-2').cloneNode(true);
+    tabs[0].setAttribute('aria-expanded', 'true');
+    tabs[1].setAttribute('aria-expanded', 'false');
+    [0, 1].forEach((index) => {
+        const panelHeading = tabPanels[index].querySelector('h3');
+        panelHeading.removeAttribute('class');
+    });
+    tabPanelSection.children[1].remove();
+
+    function selectTab() {
+        const tabIndex = parseInt(this.dataset.tabIndex);
+        if (tabs[tabIndex].getAttribute('aria-expanded') === 'false') {
+            for (let i = 0; i < tabs.length; i++) {
+                if (i === tabIndex) {
+                    tabs[i].setAttribute('aria-expanded', 'true');
+                    tabPanelSection.innerHTML = null;
+                    tabPanelSection.append(tabPanels[i]);
+                } else {
+                    tabs[i].setAttribute('aria-expanded', 'false');
+                }
+            }
+        }
+    }
+
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', selectTab);
+    });
+}, 0);

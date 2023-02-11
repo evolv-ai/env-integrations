dd007917f0c84032abd7b37cf9e6fb1982aae144
Charles Robertson <charles.robertson@evolv.ai>
2023-02-11 09:43:43 -0500

    Convert async/await to promise

--- catalyst/src/interval-poll.js
+++ catalyst/src/interval-poll.js
@@ -1,90 +1,92 @@
 function initializeIntervalPoll(catalyst) {
-    function processQueue(sandbox) {
-        const queue = sandbox._intervalPoll.queue;
+  function processQueue(sandbox) {
+    const queue = sandbox._intervalPoll.queue;
 
-        for (let i = 0; i < queue.length; i++) {
-            const entry = queue[i];
-            let timeElapsed = performance.now() - entry.startTime;
+    for (let i = 0; i < queue.length; i++) {
+      const entry = queue[i];
+      let timeElapsed = performance.now() - entry.startTime;
 
-            if (entry.timeout && entry.timeout < timeElapsed) {
-                sandbox.debug('waitUntil: condition timed out', entry);
-                queue.splice(i, 1);
-                continue;
-            }
+      if (entry.timeout && entry.timeout < timeElapsed) {
+        sandbox.debug('waitUntil: condition timed out', entry);
+        queue.splice(i, 1);
+        continue;
+      }
 
-            try {
-                if (entry.condition()) {
-                    sandbox.debug(
-                        'waitUntil: condition met:',
-                        entry.condition,
-                        entry.condition(),
-                        `${(performance.now() - entry.startTime).toFixed(2)}ms`
-                    );
-                    entry.callback();
-                    queue.splice(i, 1);
-                }
-            } catch (error) {
-                // Prevents 60 error messages per second if the condition contains an error
-                sandbox.warn(
-                    'waitUntil: error in condition, removing from queue',
-                    error
-                );
-                queue.splice(i, 1);
-            }
+      try {
+        if (entry.condition()) {
+          sandbox.debug(
+            'waitUntil: condition met:',
+            entry.condition,
+            entry.condition(),
+            `${(performance.now() - entry.startTime).toFixed(2)}ms`,
+          );
+          entry.callback();
+          queue.splice(i, 1);
         }
+      } catch (error) {
+        // Prevents 60 error messages per second if the condition contains an error
+        sandbox.warn(
+          'waitUntil: error in condition, removing from queue',
+          error,
+        );
+        queue.splice(i, 1);
+      }
     }
+  }
 
-    function processQueues() {
-        return new Promise((resolve) => {
-            const processQueuesLoop = () => {
-                let anySandboxActive = false;
-                let queueTotal = 0;
+  function processQueues() {
+    return new Promise((resolve) => {
+      const intervalPoll = catalyst._intervalPoll;
+      if (intervalPoll.isPolling) return;
+      intervalPoll.isPolling = true;
+      catalyst.debug('interval poll: start polling');
+      function done() {
+        catalyst.debug('interval poll: stop polling');
+        intervalPoll.isPolling = false;
+      }
 
-                for (const sandbox of catalyst.sandboxes) {
-                    if (sandbox._evolvContext.state === 'inactive') continue;
-                    anySandboxActive = true;
+      const processQueuesLoop = () => {
+        let anySandboxActive = false;
+        let queueTotal = 0;
 
-                    processQueue(sandbox);
-                    queueTotal += sandbox._intervalPoll.queue.length;
-                }
+        for (const sandbox of catalyst.sandboxes) {
+          if (sandbox._evolvContext.state === 'inactive') continue;
+          anySandboxActive = true;
 
-                if (!anySandboxActive) {
-                    catalyst.debug('interval poll: no active sandboxes');
-                    return resolve(false);
-                } else if (queueTotal === 0) {
-                    catalyst.debug('interval poll: all queues empty');
-                    return resolve(false);
-                } else {
-                    requestAnimationFrame(processQueuesLoop);
-                }
-            };
-            processQueuesLoop();
-        });
-    }
+          processQueue(sandbox);
+          queueTotal += sandbox._intervalPoll.queue.length;
+        }
 
-    return {
-        isPolling: false,
-        startPolling: async function () {
-            const intervalPoll = catalyst._intervalPoll;
-            if (intervalPoll.isPolling) return;
-            catalyst.debug('interval poll: start polling');
-            intervalPoll.isPolling = true;
-            intervalPoll.isPolling = await processQueues();
-            catalyst.debug('interval poll: stop polling');
-        },
-    };
+        if (!anySandboxActive) {
+          catalyst.debug('interval poll: no active sandboxes');
+          return resolve(done());
+        } else if (queueTotal === 0) {
+          catalyst.debug('interval poll: all queues empty');
+          return resolve(done());
+        } else {
+          requestAnimationFrame(processQueuesLoop);
+        }
+      };
+      processQueuesLoop();
+    });
+  }
+
+  return {
+    isPolling: false,
+    startPolling: processQueues,
+  };
 }
 
 function initializeSandboxIntervalPoll(sandbox) {
-    return {
-        queue: [],
-        reset: () => {
-            if (sandbox._intervalPoll.queue.length > 0) {
-                sandbox.debug('interval poll: clear queue');
-                sandbox._intervalPoll.queue = [];
-            }
-        },
-    };
+  return {
+    queue: [],
+    reset: () => {
+      if (sandbox._intervalPoll.queue.length > 0) {
+        sandbox.debug('interval poll: clear queue');
+        sandbox._intervalPoll.queue = [];
+      }
+    },
+  };
 }
 
 export { initializeIntervalPoll, initializeSandboxIntervalPoll };

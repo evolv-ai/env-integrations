dfe6abf3ce154d3ff07a012fae7a5cb48a34cccc
Charles Robertson <charles.robertson@evolv.ai>
2023-02-11 13:37:13 -0500

    Enforce ESLint

--- catalyst/src/catalyst.js
+++ catalyst/src/catalyst.js
@@ -1,112 +1,111 @@
-import { initializeSandbox } from './sandbox';
-import { initializeIntervalPoll } from './interval-poll';
+import initializeSandbox from './sandbox.js';
+import { initializeIntervalPoll } from './interval-poll.js';
 
 export function initializeCatalyst() {
-    const catalyst = initializeSandbox('catalyst');
-    const debug = catalyst.debug;
+  const catalyst = initializeSandbox('catalyst');
+  const { debug } = catalyst;
 
-    catalyst.sandboxes = [];
+  catalyst.sandboxes = [];
 
-    // Creates proxy for window.evolv.catalyst that adds a new sandbox whenever
-    // a new property is accessed.
-    let catalystProxy = new Proxy(catalyst, {
-        get(target, name, receiver) {
-            let catalystReflection = Reflect.get(target, name, receiver);
-            if (!catalystReflection) {
-                const sandbox = initializeSandbox(name);
-                let hasInitializedActiveKeyListener = false;
+  // Creates proxy for window.evolv.catalyst that adds a new sandbox whenever
+  // a new property is accessed.
+  const catalystProxy = new Proxy(catalyst, {
+    get(catalystTarget, name, receiver) {
+      let catalystReflection = Reflect.get(catalystTarget, name, receiver);
+      if (!catalystReflection) {
+        const sandbox = initializeSandbox(name);
+        let hasInitializedActiveKeyListener = false;
 
-                // Automatically initializes the active key listener for SPA handling if either
-                // property is set. Only permitted to run once. isActive() is deprecated.
-                const sandboxProxy = new Proxy(sandbox, {
-                    set(target, property, value) {
-                        target[property] = value;
+        // Automatically initializes the active key listener for SPA handling if either
+        // property is set. Only permitted to run once. isActive() is deprecated.
+        const sandboxProxy = new Proxy(sandbox, {
+          set(sandboxTarget, property, value) {
+            /* eslint-disable-next-line */
+            sandboxTarget[property] = value;
 
-                        if (
-                            !hasInitializedActiveKeyListener &&
-                            (property === 'id' || property === 'isActive')
-                        ) {
-                            sandbox._evolvContext.initializeActiveKeyListener(
-                                value
-                            );
-                            hasInitializedActiveKeyListener = true;
-                        } else if (
-                            property === 'id' ||
-                            property === 'isActive'
-                        ) {
-                            sandbox.debug(
-                                'init sandbox: active key listener already initialized'
-                            );
-                        }
-
-                        return true;
-                    },
-                });
-                target[name] = sandboxProxy;
-                catalystReflection = Reflect.get(target, name, receiver);
-                catalyst.sandboxes.push(sandboxProxy);
+            if (
+              !hasInitializedActiveKeyListener &&
+              (property === 'id' || property === 'isActive')
+            ) {
+              sandbox._evolvContext.initializeActiveKeyListener(value);
+              hasInitializedActiveKeyListener = true;
+            } else if (property === 'id' || property === 'isActive') {
+              sandbox.debug(
+                'init sandbox: active key listener already initialized',
+              );
             }
 
-            return catalystReflection;
-        },
-    });
+            return true;
+          },
+        });
+        /* eslint-disable-next-line */
+        catalystTarget[name] = sandboxProxy;
+        catalystReflection = Reflect.get(catalystTarget, name, receiver);
+        catalyst.sandboxes.push(sandboxProxy);
+      }
 
-    catalyst._intervalPoll = initializeIntervalPoll(catalyst);
+      return catalystReflection;
+    },
+  });
 
-    // The main mutation observer for all sandboxes
-    catalyst._globalObserver = {
-        observer: new MutationObserver(() => {
-            let anySandboxActive = false;
-            for (const sandbox of catalyst.sandboxes) {
-                if (sandbox._evolvContext.state === 'inactive') continue;
-                anySandboxActive = true;
-                sandbox.instrument.debouncedProcessQueue();
-            }
-            if (!anySandboxActive) {
-                debug('global observer: no sandboxes active');
-                catalyst._globalObserver.disconnect();
-            }
-        }),
-        connect: () => {
-            debug('global observer: observe');
-            catalyst._globalObserver.observer.observe(document.body, {
-                childList: true,
-                attributes: true,
-                subtree: true,
-            });
-            catalyst._globalObserver.state = 'active';
-        },
-        disconnect: () => {
-            debug('global observer: disconnect');
-            catalyst._globalObserver.observer.disconnect();
-            catalyst._globalObserver.state = 'inactive';
-        },
-        state: 'inactive',
-    };
+  catalyst._intervalPoll = initializeIntervalPoll(catalyst);
 
-    // catalyst._globalObserver.connect()
+  // The main mutation observer for all sandboxes
+  catalyst._globalObserver = {
+    observer: new MutationObserver(() => {
+      let anySandboxActive = false;
+      catalyst.sandboxes.forEach((sandbox) => {
+        if (sandbox._evolvContext.state === 'inactive') return;
+        anySandboxActive = true;
+        sandbox.instrument.debouncedProcessQueue();
+      });
+      if (!anySandboxActive) {
+        debug('global observer: no sandboxes active');
+        catalyst._globalObserver.disconnect();
+      }
+    }),
+    connect: () => {
+      debug('global observer: observe');
+      catalyst._globalObserver.observer.observe(document.body, {
+        childList: true,
+        attributes: true,
+        subtree: true,
+      });
+      catalyst._globalObserver.state = 'active';
+    },
+    disconnect: () => {
+      debug('global observer: disconnect');
+      catalyst._globalObserver.observer.disconnect();
+      catalyst._globalObserver.state = 'inactive';
+    },
+    state: 'inactive',
+  };
 
-    return catalystProxy;
+  // catalyst._globalObserver.connect()
+
+  return catalystProxy;
 }
 
 export function processConfig(config) {
-    function pageMatch(page) {
-        if (!page) return false;
+  function pageMatch(page) {
+    if (!page) return false;
 
-        return new RegExp(page).test(location.pathname);
-    }
+    return new RegExp(page).test(window.location.pathname);
+  }
 
-    window.evolv = window.evolv || {};
-    var pages = config && config.pages ? config.pages : ['.*'];
-    var matches = pages.some(pageMatch);
-    var evolv = window.evolv;
+  window.evolv = window.evolv || {};
+  const pages = config && config.pages ? config.pages : ['.*'];
+  const matches = pages.some(pageMatch);
+  const { evolv } = window;
 
-    if (matches) {
-        if (window.evolv.catalyst) return window.evolv.catalyst;
+  if (matches) {
+    if (window.evolv.catalyst) return window.evolv.catalyst;
 
-        evolv.catalyst = initializeCatalyst();
-        evolv.renderRule = evolv.catalyst;
+    evolv.catalyst = initializeCatalyst();
+    evolv.renderRule = evolv.catalyst;
 
-        return evolv.catalyst;
-    }
+    return evolv.catalyst;
+  }
+
+  return true;
 }
--- catalyst/src/sandbox.js
+++ catalyst/src/sandbox.js
@@ -1,64 +1,67 @@
 import { version } from '../package.json';
-import { initializeLogs } from './logs';
-import { select, selectAll } from './enode';
-import { initializeSelectInstrument, initializeInstrument } from './instrument';
-import { initializeEvolvContext, initializeTrack } from './evolv-context';
-import { initializeSandboxIntervalPoll } from './interval-poll';
-import { initializeStore } from './store';
+import { initializeLogs } from './logs.js';
+import { initializeENode } from './enode2.js';
 import {
-    initializeWhenContext,
-    initializeWhenMutate,
-    initializeWhenDOM,
-    initializeWhenItem,
-    initializeWhenElement,
-    initializeWhenElements,
-    initializeWaitUntil,
+  initializeSelectInstrument,
+  initializeInstrument,
+} from './instrument.js';
+import { initializeEvolvContext, initializeTrack } from './evolv-context.js';
+import { initializeSandboxIntervalPoll } from './interval-poll.js';
+import { initializeStore } from './store.js';
+import {
+  initializeWhenContext,
+  initializeWhenMutate,
+  initializeWhenDOM,
+  initializeWhenItem,
+  initializeWhenElement,
+  initializeWhenElements,
+  initializeWaitUntil,
 } from './when.js';
 
 function initializeSandbox(name) {
-    const sandbox = {};
-    sandbox.name = name;
+  const sandbox = {};
+  sandbox.name = name;
 
-    initializeLogs(sandbox);
-    const log = sandbox.log;
-    const debug = sandbox.debug;
+  initializeLogs(sandbox);
+  const { log, debug } = sandbox;
 
-    if (name === 'catalyst') {
-        log(`init catalyst version ${version}`);
-        log(`log level: ${sandbox.logs}`);
-        sandbox.version = version;
-    } else {
-        debug(`init context sandbox: ${name}`);
-        if (window.evolv.catalyst._globalObserver.state === 'inactive')
-            window.evolv.catalyst._globalObserver.connect();
-    }
+  if (name === 'catalyst') {
+    log(`init catalyst version ${version}`);
+    log(`log level: ${sandbox.logs}`);
+    sandbox.version = version;
+  } else {
+    debug(`init context sandbox: ${name}`);
+    if (window.evolv.catalyst._globalObserver.state === 'inactive')
+      window.evolv.catalyst._globalObserver.connect();
+  }
 
-    sandbox.$ = selectAll;
-    sandbox.select = select;
-    sandbox.selectAll = selectAll;
+  const enode = initializeENode(sandbox);
+  sandbox.$ = enode.selectAll;
+  sandbox.select = enode.select;
+  sandbox.selectAll = enode.selectAll;
 
-    if (sandbox.name !== 'catalyst') {
-        sandbox.selectInstrument = initializeSelectInstrument(sandbox);
-        sandbox.$$ = sandbox.selectInstrument;
-        sandbox.store = initializeStore(sandbox);
-        sandbox.app = {};
-        sandbox.instrument = initializeInstrument(sandbox);
-        sandbox._intervalPoll = initializeSandboxIntervalPoll(sandbox);
-        sandbox._evolvContext = initializeEvolvContext(sandbox);
-        sandbox.whenContext = initializeWhenContext(sandbox);
-        sandbox.whenMutate = initializeWhenMutate(sandbox);
-        sandbox.whenDOM = initializeWhenDOM(sandbox);
-        sandbox.whenItem = initializeWhenItem(sandbox);
-        sandbox.whenElement = initializeWhenElement(sandbox);
-        sandbox.whenElements = initializeWhenElements(sandbox);
-        sandbox.waitUntil = initializeWaitUntil(sandbox);
-        sandbox.track = initializeTrack(sandbox);
-    }
+  if (sandbox.name !== 'catalyst') {
+    sandbox.selectInstrument = initializeSelectInstrument(sandbox);
+    sandbox.$$ = sandbox.selectInstrument;
+    sandbox.store = initializeStore(sandbox);
+    sandbox.app = {};
+    sandbox.instrument = initializeInstrument(sandbox);
+    sandbox._intervalPoll = initializeSandboxIntervalPoll(sandbox);
+    sandbox._evolvContext = initializeEvolvContext(sandbox);
+    sandbox.whenContext = initializeWhenContext(sandbox);
+    sandbox.whenMutate = initializeWhenMutate(sandbox);
+    sandbox.whenDOM = initializeWhenDOM(sandbox);
+    sandbox.whenItem = initializeWhenItem(sandbox);
+    sandbox.whenElement = initializeWhenElement(sandbox);
+    sandbox.whenElements = initializeWhenElements(sandbox);
+    sandbox.waitUntil = initializeWaitUntil(sandbox);
+    sandbox.track = initializeTrack(sandbox);
+  }
 
-    // Backward compatibility
-    sandbox.reactivate = () => {};
+  // Backward compatibility
+  sandbox.reactivate = () => {};
 
-    return sandbox;
+  return sandbox;
 }
 
-export { initializeSandbox };
+export default initializeSandbox;

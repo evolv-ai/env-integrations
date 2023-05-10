14d60e066f5479363d6187907a3cb91f8f482297
rcowin <richard.cowin@gmail.com>
2023-05-10 14:08:18 -0500

    tweak spa

--- audience/src/audience.js
+++ audience/src/audience.js
@@ -1,9 +1,59 @@
 import { adapters } from './adapters.js';
 import { resolveValue } from './storage.js';
-import { Spa } from './spa.js';
 
-var audience = {
-}
+let audience = {};
+
+//spa code
+export const Spa = {
+  queue: [],
+  addSpaListener: function(topKey, variable, audObj){
+    this.queue.push({
+      topKey: topKey,
+      variable: variable,
+      audObj: audObj
+    });
+  },
+  recheck: function(force){
+    var priorAudience = Object.assign({}, audience);
+    var prevLength = this.recheckQueue.length;
+    
+    this.recheckQueue.forEach(function(l){
+      addAudience(l.topKey, l.variable, l.audObj);
+    });
+    this.recheckQueue = this.recheckQueue.filter(function(l){
+      return priorAudience[l.topKey][l.variable] === audience[l.topKey][l.variable];
+    });
+    if (force || this.recheckQueue.length < prevLength){
+        refreshAudience();
+    }
+
+    if (this.recheckQueue.length === 0){
+      this.terminateRecheck();
+    }
+  },
+  terminateRecheck: function(){
+    console.info('recheck is ended', this.recheckQueue);
+    clearInterval(this.recheckInterval);
+    this.recheckInterval = 0;
+  },
+  eventHandler: function(){
+    if (this.queue.length === 0) return;
+
+    this.recheckQueue = this.queue.slice(0); 
+    this.recheck(true);
+    this.recheckInterval = setInterval(this.recheck.bind(this), 250);
+    setTimeout(this.terminateRecheck.bind(this), 2500);
+  },
+  initListener: function(){
+    var listener = this.eventHandler.bind(this);
+    window.addEventListener('popstate', listener);
+    window.addEventListener('stateupdate_evolv', listener);
+  }
+};
+
+Spa.initListener();
+
+//audience code
 
 function refreshAudience(){
   try{

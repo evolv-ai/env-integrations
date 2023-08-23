f6e5f283547d693837fe5ca69293cb490cdeb322
rcowin <richard.cowin@gmail.com>
2023-08-23 11:32:48 -0500

    moved harness

---
+++ metrics/harness/harness.js
@@ -0,0 +1,46 @@
+
+//nuf said
+////////////////////////////////////////////////
+import {processConfig} from '../src/metrics.js';
+// import data from './a&e.json';
+// import data from ./acc.json';
+import data from './test.json';
+// import data from './safelite.json';
+// import data from './ndr.json';
+
+function loadScript(path){
+  var scriptNode = document.createElement('script');
+  scriptNode.setAttribute('src', path);
+  document.head.appendChild(scriptNode);
+}
+
+function waitFor(check, invoke, poll){
+  if (check()){
+      invoke();
+      return;
+  }
+  var polling = setInterval(function(){
+    try{
+      if (check()){
+        invoke();
+        clearInterval(polling);
+        polling = null;
+      }
+    } catch(e){console.info('listener not processed')}
+  }, poll.interval)
+  setTimeout(function(){ 
+      if (!polling) return
+      
+      clearInterval(polling)     
+      console.info('evolv render listener timeout', poll)
+      window.evolvRenderTimeout = {
+          msg:'evolv render listener timeout', poll: poll
+      }
+  }, poll.duration)
+}
+
+waitFor(
+  () => window.evolv && window.evolv.context && window.evolv.collect, 
+  ()=> processConfig(data),
+  {duration: 900000, interval:20}
+)

4b2880ad283249ffb5df509038365d658ca5cd4a
Charles Robertson <charles.robertson@evolv.ai>
2024-03-20 15:57:00 -0400

    task: add multi-device support

--- device-data-pdp/src/index.js
+++ device-data-pdp/src/index.js
@@ -25,6 +25,8 @@ export default function (config) {
     const { collect, mutate, $mu } = window.evolv;
     const sessionKey = 'evolv:device-data-pdp';
     const webURL = 'web.url';
+    let dpHasLoaded = false;
+    let cartHasLoaded = false;
 
     // Procedure if requirements are not met
     function fail(message) {
@@ -40,32 +42,49 @@ export default function (config) {
     function checkURL(event, key, url) {
       if (!(key === webURL)) return;
 
-      if (/\/smartphones\/(?!.*-certified-pre-owned).*\//.test(url)) {
+      if (/\/smartphones\/(?!.*-certified-pre-owned).*\//i.test(url)) {
         pdpPage();
-      } else if (/\/sales\/nextgen\/protection(\/options)?\.html/.test(url)) {
+      } else if (/\/sales\/nextgen\/protection(\/options)?\.html/i.test(url)) {
         dpPages();
+      } else if (/\/sales\/nextgen\/expresscart\.html/i.test(url)) {
+        cartPage();
       }
+    }
+
+    function updateSessionStorage(lineIndex) {
+      let deviceValues;
+
+      const deviceValuesRaw = window.vzdl?.txn?.product?.current?.filter(product => product.category.toLowerCase() === 'device')?.map(product => {
+        return (({ name, nonRecurringPrice, productId }) => ({
+            name,
+            nonRecurringPrice,
+            productId,
+          }))(product)
+      });
+
+      if (lineIndex >= 0) {
+        const deviceValuesArray = [];
+        deviceValuesArray[lineIndex] = deviceValuesRaw[0];
+        deviceValues = JSON.stringify(deviceValuesArray);
+      } else {
+        deviceValues = JSON.stringify(deviceValuesRaw);
+      }
+
+      if (!deviceValues || deviceValues.length < 1) return;
+
+      log(
+        `Set sessionStorage item '${sessionKey}' to ${deviceValues}`
+      );
+
+      sessionStorage.setItem(sessionKey, deviceValues);
     }
 
     function pdpPage() {
       log(`init: pdp page - v${version}`);
-      // const exampleValue = true;
+      const mtn = sessionStorage.getItem('SELECTED_PROSPECT_MTN');
 
-      waitFor(() => window.vzdl?.txn?.product?.current?.[0]).then(
-        (deviceValues) => {
-          const deviceValuesEdited = JSON.stringify(
-            (({ name, nonRecurringPrice, productId }) => ({
-              name,
-              nonRecurringPrice,
-              productId,
-            }))(deviceValues)
-          );
-          log(
-            `Set sessionStorage item '${sessionKey}' to ${deviceValuesEdited}`
-          );
-          sessionStorage.setItem(sessionKey, deviceValuesEdited);
-        }
-      );
+      const lineIndex = mtn ? parseInt( mtn.match(/newLine(\d{1,})/)?.[1], 10) : 0;
+      waitFor(() => window.vzdl?.txn?.product?.current?.[0]).then(() => updateSessionStorage(lineIndex));
     }
 
     function dpPages() {
@@ -76,17 +95,36 @@ export default function (config) {
         fail(`Session storage item '${sessionKey}' was not found`);
       }
 
-      // sessionStorage is always a string, so convert to boolean
-      const deviceValuesObj = JSON.parse(deviceValues);
+      if (dpHasLoaded) return;
 
-      Object.keys(deviceValuesObj).forEach((key) => {
-        const contextKey = `vz.device.${key}`;
-        const contextValue = deviceValuesObj[key];
-        log(
-          `Bind '${contextKey}': '${contextValue}' to evolv.context.remoteContext`
-        );
-        window.evolv.context.set(contextKey, contextValue);
-      });
+      $mu('#stickydevice-device-line-info', 'line').customMutation((state, lineElement) => {
+        const lineIndex = parseInt(lineElement.textContent.match(/Line (\d{1,})/)?.[1], 10) - 1;
+        if (!(lineIndex >= 0)) fail('No line number');
+
+        const deviceValuesObj = JSON.parse(deviceValues);
+        const device = deviceValuesObj[lineIndex];
+
+        Object.keys(device).forEach((key) => {
+          const contextKey = `vz.device.${key}`;
+          const contextValue = (key === 'nonRecurringPrice') ? parseFloat(device[key]) : device[key];
+
+          log(
+            `Bind '${contextKey}': '${contextValue}' to evolv.context.remoteContext`
+          );
+          window.evolv.context.set(contextKey, contextValue);
+        });
+      })
+
+      dpHasLoaded = true;
+    }
+
+    function cartPage() {
+      log(`init: cart page - v${version}`);
+
+      if (cartHasLoaded) return;
+      $mu('.cart', 'int-device-data-pdp-device-watcher').customMutation(updateSessionStorage, updateSessionStorage);
+
+      cartHasLoaded = true;
     }
 
     checkURL('init', webURL, evolv.context.get(webURL));

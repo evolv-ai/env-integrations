04dbbee04162b6d0520fca849ee6667d8c95eb21
Charles Robertson <charles.robertson@evolv.ai>
2024-03-28 15:31:53 -0400

    task: write integration

--- progressive-plans-audience/src/index.js
+++ progressive-plans-audience/src/index.js
@@ -1,6 +1,6 @@
 import { version } from '../package.json';
 
-module.exports = (config) => {
+export default (config) => {
   function waitFor(callback, timeout = 5000, interval = 25) {
     return new Promise((resolve, reject) => {
         let poll;
@@ -20,13 +20,13 @@ module.exports = (config) => {
   }
 
   waitFor(() => window.evolv?.utils).then(utilsGlobal => {
-    utilsGlobal.init('int-example');
-    const utils = utilsGlobal['int-example'];
+    const sandboxKey = 'int-progressive-plans-audience';
+    const utils = utilsGlobal.init(sandboxKey);
     const { log, debug, warn } = utils;
     const { collect, mutate, $mu } = window.evolv;
-    const sessionKey = 'evolv:example-value';
-    const contextKey = 'vz.exampleValue';
+    const contextKey = 'vz.AAL-banner';
     const webURL = 'web.url';
+    utils.hasRun ??= false;
 
     // Procedure if requirements are not met
     function fail(message) {
@@ -35,43 +35,75 @@ module.exports = (config) => {
       sessionStorage.removeItem(sessionKey);
       window.evolv.client.contaminate({
         reason: 'requirements-unmet',
-        details: message,
+        details: `${sandboxKey} ${message}`,
       });
+    }
+
+    function isProgressivePlansPage(url = window.location.href) {
+      return /\/sales\/nextgen\/plans\/progressiveplans\.html/i.test(url) //sales/nextgen/plans/progressiveplans.html
     }
 
     function checkURL(event, key, url) {
       if (!(key === webURL)) return;
 
-      if (/\/example\/page\/1\//.test(url)) {
-        examplePage1();
-      } else if (/\/example\/page\/2\//.test(url)) {
-        examplePage2();
+      if (isProgressivePlansPage(url)) {
+        progressivePlansPage();
       }
     }
 
-    function examplePage1() {
-      log(`init: example page 1 - v${version}`);
-      const exampleValue = true;
+    function progressivePlansPage() {
+      log(`init: progressive plans page - v${version}`);
 
-      waitFor(() => exampleValue).then(exampleValue => {
-        log(`Set sessionStorage item '${sessionKey}' to ${exampleValue}`);
-        sessionStorage.setItem(sessionKey, exampleValue);
-      });
-    }
+      if (utils.hasRun) return;
 
-    function examplePage2() {
-      log(`init: example page 2 - v${version}`);
-      const exampleValueString = sessionStorage.getItem(sessionKey);
+      const planReference = [
+        {
+          'Unlimited Ultimate': 90,
+          'Unlimited Plus': 80,
+          'Unlimited Welcome': 65,
+        },
+        {
+          'Unlimited Ultimate': 75,
+          'Unlimited Plus': 65,
+          'Unlimited Welcome': 50,
+        },
+        {
+          'Unlimited Ultimate': 65,
+          'Unlimited Plus': 55,
+          'Unlimited Welcome': 40,
+        }
+      ]
 
-      if (!exampleValueString) {
-        fail(`Session storage item '${sessionKey}' was not found`);
-      }
+      const planPricing = {}
 
-      // sessionStorage is always a string, so convert to boolean
-      const exampleValue = exampleValueString.toLowerCase === 'true'; 
+      $mu('//section[@data-testid="test-select-data"]//div[contains(@id,"carousel")][contains(@class,"slot")]//h2/ancestor::div[2]', 'int-progressive-plan-audience-tile-heading')
+        .customMutation((state, tileHeading) => {
+          if (!isProgressivePlansPage) return;
 
-      log(`Bind '${contextKey}' to evolv.context.remoteContext`);
-      window.evolv.context.set(contextKey, exampleValue);
+          const planPrices = {};
+          const planName = tileHeading.querySelector('h2').textContent;
+          const planPrice = parseInt(tileHeading.querySelector('span[regularweight="light"] span').textContent.match(/\$(\d+)/)[1], 10);
+          const planAutoPayText = Array.from(tileHeading.querySelectorAll('span')).find(span => /With(out)? Auto Pay/.test(span.textContent)).textContent;
+          let planAutoPay;
+
+          if (planAutoPayText?.includes('Without')) {
+            planAutoPay = false;
+          } else if (planAutoPayText?.includes('With')) {
+            planAutoPay = true;
+          } else {
+            fail('Autopay status not found')
+          }
+
+          planPricing[planName] = planAutoPay ? planPrice : planPrice + 10;
+          
+          if (Object.keys(planPricing).length === 3) {
+            const planReferenceIndex = planReference.findIndex(planGroup => Object.keys(planPricing).every(name => planPricing[name] === planGroup[name]))
+            log(`Bind '${contextKey}' to evolv.context.remoteContext`);
+            window.evolv.context.set(contextKey, planReferenceIndex);
+          }
+
+          utils.hasRun = true;
+        });
     }
   
     checkURL('init', webURL, evolv.context.get(webURL));

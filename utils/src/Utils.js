2df994f233a8973055d53ec6e2f203d1b45f4935
Charles Robertson <charles.robertson@evolv.ai>
2025-08-12 14:55:11 -0400

    feat: utils: add exclusion groups

--- utils/src/Utils.js
+++ utils/src/Utils.js
@@ -98,6 +98,23 @@ class Utils {
         });
     }
 
+    if (config?.exclusion_group) {
+      const exclusionGroups = sessionStorage.getItem('evolv:exclusion-groups')?.split('|') || [];
+      const exclusionValue = `${config.exclusion_group}:${config.id || config.contexts[0].id}`;
+
+      if (exclusionGroups.some(entry =>
+        (entry !== exclusionValue) && new RegExp(`^${config.exclusion_group}:`).test(entry)
+      )) {
+        this.fail(`Evolv: Test improperly excluded. Exclusion value: '${exclusionValue}', exclusion groups: ${exclusionGroups.join('|')}`, 'improper-exclusion');
+      }
+
+      if (!exclusionGroups.includes(exclusionValue)) {
+        exclusionGroups.push(exclusionValue);
+        this.log(`exclusion group: set exclusion for '${exclusionValue}'`);
+        sessionStorage.setItem('evolv:exclusion-groups', exclusionGroups.join('|'));
+      }
+    }
+
     this.toRevert = [];
     this.#described = [];
     this.isNewConfig = this.config && !this.config.contexts && this.config.id;
